c1e3f6ba894356018621900f48699336228f6e7d
Huda <hudaelzubair@hotmail.com>
2024-06-30 10:51:44 +0300

    fixes

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,14 +9,17 @@ import { Weather } from './weather';
 export class WeatherService {
 
   constructor(private http:HttpClient) { }
-  private apiUrl='api.openweathermap.org/data/2.5/';
-  private apiKey='<a7df637e2fba570e556b298e042ef86a>';
+   //private apiUrl='api.openweathermap.org/data/2.5/';
+  //  private apiUrl='https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=';
+  // private apiUrl='https://api.open-meteo.com/v1/dwd-icon?latitude=52.52&longitude=13.41&hourly=temperature_2m';
+  //  private apiKey='<a7df637e2fba570e556b298e042ef86a>';
+  //  private apiKey='9ab5349f543716e3e7435dc2ec7ff158';
 
   getWeather(city:string):Observable<Weather>{
     const options =new HttpParams()
     .set('units','metrics')
     .set('q',city)
-    .set('appId',this.apiKey);
+     .set('appId',this.apiKey);
     return this.http.get<Weather>(this.apiUrl+'weather',{params:
       options});
   }
